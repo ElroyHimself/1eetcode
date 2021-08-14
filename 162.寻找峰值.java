86e6b2a0f959fb37aa4ccdadff647a6b0fc31b37
ElroyHimself <1102492250@qq.com>
2021-08-14 23:45:17 +0800

    二分查找

--- 162.寻找峰值.java
+++ 162.寻找峰值.java
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-14 23:13:19
- * @LastEditTime: 2021-08-14 23:40:41
+ * @LastEditTime: 2021-08-14 23:44:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \.leetcode\162.寻找峰值.java
@@ -13,6 +13,12 @@
  */
 
 // @lc code=start
+/** 
+ * 
+nums[mid] > max(nums[mid-1], nums[mid+1])	mid 即为所求
+nums[mid] < nums[mid+1]			            [mid+1, n-1] 范围内必然有一个峰值
+nums[mid] < nums[mid-1]				        [0, mid-1] 范围内必然有一个峰值
+*/
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length-1;
